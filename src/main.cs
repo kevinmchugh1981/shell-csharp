bccc1e4b264d83f88d1233f37fcea804fc60954d
Kevin <kevin.mchugh81@googlemail.com>
2026-02-11 12:28:06 +0000

    Task Six

--- src/main.cs
+++ src/main.cs
@@ -12,6 +12,17 @@ class Program
             if (!string.IsNullOrWhiteSpace(input) &&
                 input.StartsWith("echo", StringComparison.InvariantCultureIgnoreCase))
                 Console.WriteLine(input.Replace("echo ", string.Empty));
+            else if (!string.IsNullOrWhiteSpace(input) &&
+                     input.StartsWith("type", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var target = input.Replace("type ", string.Empty);
+                if (!string.IsNullOrWhiteSpace(target) && (target.Equals("echo", StringComparison.InvariantCultureIgnoreCase) ||
+                                                           target.Equals("type", StringComparison.InvariantCultureIgnoreCase) ||
+                                                           target.Equals("exit", StringComparison.InvariantCultureIgnoreCase)))
+                    Console.WriteLine($"{target} is a shell builtin");
+                else
+                    Console.WriteLine($"{target}: not found");
+            }
             else 
                 Console.WriteLine($"{input}: command not found");
         }
