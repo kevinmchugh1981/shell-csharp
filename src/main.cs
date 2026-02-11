3a02743189d43d4af178ca365706e416d9740d54
Kevin <kevin.mchugh81@googlemail.com>
2026-02-11 12:22:46 +0000

    Task Five

--- src/main.cs
+++ src/main.cs
@@ -9,6 +9,8 @@ class Program
             var input = Console.ReadLine();
             if (!string.IsNullOrWhiteSpace(input) && input.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
                 return;
+            if(!string.IsNullOrWhiteSpace(input) && input.StartsWith("echo", StringComparison.InvariantCultureIgnoreCase))
+                Console.WriteLine(input);
             Console.WriteLine($"{input}: command not found");    
         }
     }
