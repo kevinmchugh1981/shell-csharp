fbeca9729c487fbc85284f80eab487b09416dc5f
Kevin <kevin.mchugh81@googlemail.com>
2026-02-11 13:10:30 +0000

    Task Eight

--- src/main.cs
+++ src/main.cs
@@ -1,21 +1,21 @@
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 class Program
 {
-
     private static readonly List<string> BuiltIns = [ExitName, EchoName, TypeName];
     private static string ExitName => "exit";
     private static string TypeName => "type";
     private static string EchoName => "echo";
-    
+
     static void Main()
     {
-
         while (true)
         {
             Console.Write("$ ");
             var input = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(input) && input.Equals(ExitName, StringComparison.InvariantCultureIgnoreCase))
+            if (!string.IsNullOrWhiteSpace(input) &&
+                input.Equals(ExitName, StringComparison.InvariantCultureIgnoreCase))
                 return;
             switch (string.IsNullOrWhiteSpace(input))
             {
@@ -39,28 +39,34 @@ class Program
                             Console.WriteLine($"{target}: not found");
                             break;
                     }
+
                     break;
                 }
                 default:
-                    Console.WriteLine( $"{input}: command not found");
+                    if (IsExecutable(input.Split(" ")[0], out var path))
+                        Execute(path, string.Join(string.Empty, input.Split().Skip(1)));
+                    else
+                        Console.WriteLine($"{input}: command not found");
                     break;
             }
         }
     }
+
     private static bool IsExecutable(string name, out string filePath)
     {
         filePath = string.Empty;
-        
-        if (!TryGetDirectories(out var directories))
-            return false;       
 
-        foreach (var fullPath in directories.Select(directory => Path.Combine(directory.Trim(), name)).Where(IsFileExecutable))
+        if (!TryGetDirectories(out var directories))
+            return false;
+
+        foreach (var fullPath in directories.Select(directory => Path.Combine(directory.Trim(), name))
+                     .Where(IsFileExecutable))
         {
             filePath = fullPath;
             return true;
         }
 
-        return  false;
+        return false;
     }
 
     private static bool TryGetDirectories(out List<string> directories)
@@ -73,12 +79,12 @@ class Program
         directories = pathEnv.Split(pathSeparator).ToList();
         return directories.Count > 0;
     }
-    
+
     private static bool IsFileExecutable(string filePath)
     {
         if (!File.Exists(filePath))
             return false;
-        
+
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             var ext = Path.GetExtension(filePath).ToLower();
@@ -91,5 +97,27 @@ class Program
         return (fileInfo.UnixFileMode & (UnixFileMode.UserExecute |
                                          UnixFileMode.GroupExecute |
                                          UnixFileMode.OtherExecute)) != 0;
+    }
+
+    private static void Execute(string filePath, string arguments)
+    {
+        var command = Path.GetFileName(filePath);
+        var directory = Path.GetDirectoryName(filePath);
+
+        var startInfo = new ProcessStartInfo(command, arguments)
+        {
+            WorkingDirectory = directory,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false
+        };
+        using var process = System.Diagnostics.Process.Start(startInfo);
+        var output = process?.StandardOutput.ReadToEnd();
+        var error = process?.StandardError.ReadToEnd();
+        process?.WaitForExit();
+        if (!string.IsNullOrWhiteSpace(output))
+            Console.Write(output);
+        if (!string.IsNullOrWhiteSpace(error))
+            Console.Write(error);
     }
 }
