5b6e22a8992b7d6f262817625b9c8c05b9c9b36b
Kevin <kevin.mchugh81@googlemail.com>
2026-02-14 17:28:21 +0000

    Task Sixtenn

--- src/Parsers.cs
+++ src/Parsers.cs
@@ -4,19 +4,23 @@
     private static char DoubleQuote => '"';
     private static char EscapeChar => '\\';
     
+    private static List<char> Quotes => [SingleQuote, DoubleQuote];
+    
+    
     internal static List<string> Parse(string input)
     {
         var delimiter = Char.MinValue;
         var result = new List<string>();
         var currentLine = string.Empty;
-        for (int x = 0; x < input.Length; ++x)
+        for (var x = 0; x < input.Length; ++x)
         {
             //Open quotes.
-            if (!input.PreviousElement(x).Equals(EscapeChar) && (input[x].Equals(SingleQuote) || input[x].Equals(DoubleQuote)) &&
+            if (!input.PreviousElement(x).Equals(EscapeChar) && Quotes.Contains(input[x]) &&
                 delimiter == Char.MinValue)
             {
-                //If the line isn't blank and your aren't just opening a new quote in the next char.
-                if (!string.IsNullOrWhiteSpace(currentLine) && !input.EmptyQuotes(x, input[x]))
+                //If the line isn't blank, and you aren't just opening a new quote in the next char.
+                if (!string.IsNullOrWhiteSpace(currentLine) &&  
+                    (char.IsWhiteSpace(input.NextElement(x)) || input.NextElement(x).Equals(char.MinValue)))
                 {
                     result.Add(currentLine);
                     currentLine = string.Empty;
@@ -26,11 +30,12 @@
                 continue;
             }
 
-            //Close quotes.
-            if (!input.PreviousElement(x).Equals(EscapeChar) && input[x] == delimiter)
+            //Close quotes, if you aren't being told to escape the char, or ignore the escape char if you are single quotes.
+            if ((input[x] == delimiter && !input.PreviousElement(x).Equals(EscapeChar)) ||
+                (input[x] == delimiter && input.PreviousElement(x).Equals(EscapeChar) && delimiter.Equals(SingleQuote)))
             {
                 //If the line isn't blank, and you aren't just opening a new quote in the next char.
-                if ((input.LastElement(x) || (!input.EmptyQuotes(x, input[x]))) &&
+                if (input.LastElement(x) || (char.IsWhiteSpace(input.NextElement(x)) || input.NextElement(x).Equals(char.MinValue)) &&
                     !string.IsNullOrEmpty(currentLine))
                 {
                     result.Add(currentLine);
@@ -40,7 +45,7 @@
                 delimiter = char.MinValue;
                 continue;
             }
-
+            
             //If current char is escape indicator, and you are outside quotes.
             if (input[x].Equals(EscapeChar) && delimiter.Equals(char.MinValue) && !input.PreviousElement(x).Equals(EscapeChar))
             {
