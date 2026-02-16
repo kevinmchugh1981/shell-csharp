da742023cf97dedcc75cb781e19f8a12dae4fc57
Kevin <kevin.mchugh81@googlemail.com>
2026-02-16 11:10:12 +0000

    Task Seventeen

--- src/Parsers.cs
+++ src/Parsers.cs
@@ -3,23 +3,32 @@
     private static char SingleQuote => '\'';
     private static char DoubleQuote => '"';
     private static char EscapeChar => '\\';
-    
+
     private static List<char> Quotes => [SingleQuote, DoubleQuote];
-    
-    
+
+
     internal static List<string> Parse(string input)
     {
         var delimiter = Char.MinValue;
         var result = new List<string>();
         var currentLine = string.Empty;
+        var escapeNextChar = false;
         for (var x = 0; x < input.Length; ++x)
         {
+
+            //If you find an escape char, that's not already being escaped, and you aren't inside single quotes, escape it.
+            if (!input.PreviousElement(x).Equals(EscapeChar) && input[x].Equals(EscapeChar) && !delimiter.Equals(SingleQuote))
+            {
+                escapeNextChar = true;
+                continue;
+            }
+            
             //Open quotes.
-            if (!input.PreviousElement(x).Equals(EscapeChar) && Quotes.Contains(input[x]) &&
+            if (!escapeNextChar && Quotes.Contains(input[x]) &&
                 delimiter == Char.MinValue)
             {
                 //If the line isn't blank, and you aren't just opening a new quote in the next char.
-                if (!string.IsNullOrWhiteSpace(currentLine) &&  
+                if (!string.IsNullOrWhiteSpace(currentLine) &&
                     (char.IsWhiteSpace(input.NextElement(x)) || input.NextElement(x).Equals(char.MinValue)))
                 {
                     result.Add(currentLine);
@@ -31,41 +40,43 @@
             }
 
             //Close quotes, if you aren't being told to escape the char, or ignore the escape char if you are single quotes.
-            if ((input[x] == delimiter && !input.PreviousElement(x).Equals(EscapeChar)) ||
-                (input[x] == delimiter && input.PreviousElement(x).Equals(EscapeChar) && delimiter.Equals(SingleQuote)))
+            if (input[x] == delimiter && !escapeNextChar ||
+                (input[x] == delimiter && escapeNextChar && delimiter.Equals(SingleQuote)))
             {
                 //If the line isn't blank, and you aren't just opening a new quote in the next char.
-                if (input.LastElement(x) || (char.IsWhiteSpace(input.NextElement(x)) || input.NextElement(x).Equals(char.MinValue)) &&
+                if (input.LastElement(x) ||
+                    (char.IsWhiteSpace(input.NextElement(x)) || input.NextElement(x).Equals(char.MinValue)) &&
                     !string.IsNullOrEmpty(currentLine))
                 {
                     result.Add(currentLine);
                     currentLine = string.Empty;
                 }
-                
+
                 delimiter = char.MinValue;
                 continue;
             }
             
-            //If current char is escape indicator, and you are outside quotes.
-            if (input[x].Equals(EscapeChar) && delimiter.Equals(char.MinValue) && !input.PreviousElement(x).Equals(EscapeChar))
-            {
-                continue;
-            }
-
-            //Add any char as you as inside quotes.
+            //Add chars inside quotes.
             if (delimiter != char.MinValue)
+            {
+                //Ignore escape characters
+                if (!delimiter.Equals(SingleQuote) && !input.PreviousElement(x).Equals(EscapeChar) && input[x].Equals(EscapeChar))
+                    continue;
+                
+                //Add character.
                 currentLine += input[x];
+            }
             //Outside quotes, the result isn't blank, and it's already ending in a blank space, add a char.
             else if (!string.IsNullOrWhiteSpace(currentLine) && char.IsWhiteSpace(currentLine.Last()))
             {
-                if (input.PreviousElement(x).Equals(EscapeChar) || !char.IsWhiteSpace(input[x]))
+                if (escapeNextChar || !char.IsWhiteSpace(input[x]))
                     currentLine += input[x];
             }
             //Outside of quotes.
             else
             {
                 //If the char is not a white space, or you are escaping it then add it.
-                if(input.PreviousElement(x).Equals(EscapeChar) || !char.IsWhiteSpace(input[x]))
+                if (escapeNextChar || !char.IsWhiteSpace(input[x]))
                     currentLine += input[x];
                 //If the char is a whitespace and there is a value in the current line then add it.
                 else if (char.IsWhiteSpace(input[x]) && !string.IsNullOrWhiteSpace(currentLine))
@@ -78,8 +89,12 @@
             //If you have run out of letters, add it so it's not lost.
             if (input.LastElement(x) && !string.IsNullOrEmpty(currentLine))
                 result.Add(currentLine);
+            
+            //unset escape next char.
+            escapeNextChar = false;
         }
 
         return result;
     }
+    
 }
